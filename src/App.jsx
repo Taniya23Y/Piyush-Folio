309448b7d034f683e00f2c9f4f304ddc99ae3115
Taniya Y <128471154+Taniya23Y@users.noreply.github.com>
2024-07-20 01:09:35 +0530

    Starter code

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import LocomotiveScroll from "locomotive-scroll";
+
+function App() {
+  const locomotiveScroll = new LocomotiveScroll();
+  return (
+    <>
+      <div>Hello React Developers</div>
+    </>
+  );
+}
+
+export default App;
